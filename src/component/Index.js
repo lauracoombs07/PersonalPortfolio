d7c8b95fddd289d3053142181c2316b16b525519
Laura <laura.coombs07@gmail.com>
2018-12-22 15:33:51 -0700

    Start of app

---
+++ src/component/Index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Index.css';
+
+class Index extends Component {
+  render() {
+    return (
+      <div className="Index">
+        <h1>banana</h1>
+      </div>
+    );
+  }
+}
+
+export default Index;
---
+++ src/component/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// import './Users.css';
+
+class Users extends Component {
+  render() {
+    return (
+      <div className="Users">
+        <h1>"apples"</h1>
+      </div>
+    );
+  }
+}
+
+export default Users;
